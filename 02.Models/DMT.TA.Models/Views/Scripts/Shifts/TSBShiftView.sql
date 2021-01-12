e7347127873ca3808da34e5349c0d88817ea5eae
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-13 05:46:35 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #777 - DMT.TAxTODv3   - Move project from DMT.TAxTODv2     - DMT.TA.Models (References assemblies Checked)     - DMT.TOD.Models (References assemblies Checked)

---
+++ 02.Models/DMT.TA.Models/Views/Scripts/Shifts/TSBShiftView.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW TSBShiftView
+AS
+	SELECT TSBShift.*
+		 , TSB.TSBNameEN, TSB.TSBNameTH
+		 , [Shift].ShiftNameEN, [Shift].ShiftNameTH
+	  FROM TSBShift
+		 , TSB
+	     , [Shift]
+	 WHERE TSBShift.TSBId = TSB.TSBId
+	   AND TSBShift.ShiftId = [Shift].ShiftId

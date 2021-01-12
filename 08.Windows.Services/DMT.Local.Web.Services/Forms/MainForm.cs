99131aaaedc0f74a8f92c31012236dd0dc857f01
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-13 06:16:33 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #784 - DMT.TAxTODv3   - Move project from DMT.TAxTODv1     - DMT.Local.Web.Services (References assemblies Checked)

---
+++ 08.Windows.Services/DMT.Local.Web.Services/Forms/MainForm.cs
@@ -0,0 +1,48 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Forms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private LocalDatabaseWebServer server = null;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Load Config service.
+            PlazaServiceConfigManager.Instance.LoadConfig();
+            if (null == server)
+            {
+                server = new LocalDatabaseWebServer();
+                server.Start();
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (null != server)
+            {
+                server.Shutdown();
+            }
+            server = null;
+        }
+    }
+}

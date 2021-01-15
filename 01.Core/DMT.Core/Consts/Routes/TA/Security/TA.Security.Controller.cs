93f57e006993d16b2670f51e468836cc638b9cc0
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-15 08:10:20 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #815 - DMT.TAxTODv3   - DMT.Core project #3     - Account Routes       - Security actions         - Role         - User     - TA Routes       - Security actions         - Role         - User     - TOD Routes       - Security actions         - Role         - User

--- 01.Core/DMT.Core/Consts/Routes/TA/Security/TA.Security.Controller.cs
+++ 01.Core/DMT.Core/Consts/Routes/TA/Security/TA.Security.Controller.cs
@@ -4,7 +4,45 @@
     {
         partial class TA 
         {
+            /// <summary>The Security Controller.</summary>
+            public static partial class Security
+            {
+                /// <summary>Gets controller name.</summary>
+                public const string Name = "Security";
+                /// <summary>Gets base controller url.</summary>
+                public const string Url = TA.Url + @"/" + Name;
 
+                /// <summary>The Security's Role Controller.</summary>
+                public static partial class Role
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "RoleManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Role";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Security.Url + @"/" + Name;
+                }
+
+                /// <summary>The Security's User Controller.</summary>
+                public static partial class User
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "UserManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "User";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Security.Url + @"/" + Name;
+
+                    /// <summary>The Seacch class.</summary>
+                    public static partial class Search
+                    {
+                        /// <summary>Gets route url.</summary>
+                        public const string Url = User.Url + @"/Search";
+                    }
+                }
+            }
         }
     }
 }

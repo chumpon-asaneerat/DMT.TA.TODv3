a2fb8d2c2bf45b33ad997eda915234ed1d5c28f3
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-15 08:04:45 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #814 - DMT.TAxTODv3   - DMT.Core project     - Account Routes #2       - Infrastructure actions         - TSB         - PlazaGroup         - Plaza         - Lane     - TA Routes       - Infrastructure actions         - TSB         - PlazaGroup         - Plaza         - Lane     - TOD Routes       - Infrastructure actions         - TSB         - PlazaGroup         - Plaza         - Lane

--- 01.Core/DMT.Core/Consts/Routes/TA/Infrastructure/TA.Infrastructure.Controller.cs
+++ 01.Core/DMT.Core/Consts/Routes/TA/Infrastructure/TA.Infrastructure.Controller.cs
@@ -4,7 +4,83 @@
     {
         partial class TA 
         {
+            /// <summary>The Infrastructure class.</summary>
+            public static partial class Infrastructure
+            {
+                /// <summary>Gets route name.</summary>
+                public const string Name = "Infrastructure";
+                /// <summary>Gets base controller url.</summary>
+                public const string Url = TA.Url + @"/" + Name;
 
+                /// <summary>The Infrastructure's TSB Controller.</summary>
+                public static partial class TSB
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "TSBManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "TSB";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Infrastructure.Url + @"/" + Name;
+                }
+
+                /// <summary>The Infrastructure's PlazaGroup Controller.</summary>
+                public static partial class PlazaGroup
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "PlazaGroupManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "PlazaGroup";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Infrastructure.Url + @"/" + Name;
+
+                    /// <summary>The Seacch class.</summary>
+                    public static partial class Search
+                    {
+                        /// <summary>Gets route url.</summary>
+                        public const string Url = PlazaGroup.Url + @"/Search";
+                    }
+                }
+
+                /// <summary>The Infrastructure's Plaza Controller.</summary>
+                public static partial class Plaza
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "PlazaManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Plaza";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Infrastructure.Url + @"/" + Name;
+
+                    /// <summary>The Seacch class.</summary>
+                    public static partial class Search
+                    {
+                        /// <summary>Gets route url.</summary>
+                        public const string Url = Plaza.Url + @"/Search";
+                    }
+                }
+
+                /// <summary>The Infrastructure's Lane Controller.</summary>
+                public static partial class Lane
+                {
+                    /// <summary>Gets controller name.</summary>
+                    public const string ControllerName = "LaneManage";
+
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Lane";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = Infrastructure.Url + @"/" + Name;
+
+                    /// <summary>The Seacch class.</summary>
+                    public static partial class Search
+                    {
+                        /// <summary>Gets route url.</summary>
+                        public const string Url = Lane.Url + @"/Search";
+                    }
+                }
+            }
         }
     }
 }

b9db20730bed643b60660b9950b1f673bdc23c38
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-18 04:00:43 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #881 - DMT.TAxTODv3   - DMT.TA.App project #1.     - Update All StatusBar elements.     - Update App.xaml.cs     - Update UIConfigs.cs   - DMT.TOD.App project #1.     - Update All StatusBar elements.     - Update App.xaml.cs     - Update UIConfigs.cs

---
+++ 09.App/DMT.TA.App/Services/UIConfigs.cs
@@ -0,0 +1,182 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace DMT.Configurations
+{
+    #region TAUIConfig
+
+    /// <summary>
+    /// The TAUIConfig class.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    public class TAUIConfig
+    {
+        #region Constructor
+
+        /// <summary>
+        /// TAUIConfig.
+        /// </summary>
+        public TAUIConfig()
+        {
+            this.AppInfo = new StatusBarConfig() { Name = "AppInfo", Visible = true };
+            this.ClientInfo = new StatusBarConfig() { Name = "ClientInfo", Visible = true };
+            this.LocalDb = new StatusBarConfig() { Name = "LocalDb", Visible = false };
+            this.SCW = new StatusBarConfig() { Name = "SCW", Visible = false };
+            this.TAServer = new StatusBarConfig() { Name = "TAServer", Visible = true };
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets or sets AppInfo status bar.</summary>
+        public StatusBarConfig AppInfo { get; set; }
+        /// <summary>Gets or sets ClientInfo status bar.</summary>
+        public StatusBarConfig ClientInfo { get; set; }
+        /// <summary>Gets or sets LocalDb status bar.</summary>
+        public StatusBarConfig LocalDb { get; set; }
+        /// <summary>Gets or sets SCW status bar.</summary>
+        public StatusBarConfig SCW { get; set; }
+        /// <summary>Gets or sets TAServer status bar.</summary>
+        public StatusBarConfig TAServer { get; set; }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region TAUIConfigManager
+
+    /// <summary>
+    /// TA UI Config Manager class.
+    /// </summary>
+    public class TAUIConfigManager : JsonConfigFileManger<TAUIConfig>
+    {
+        #region Static Instance Access
+
+        private static TAUIConfigManager _instance = null;
+
+        /// <summary>
+        /// Gets ConfigManager instance access.
+        /// </summary>
+        public static TAUIConfigManager Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(TAUIConfigManager))
+                    {
+                        _instance = new TAUIConfigManager();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private string _fileName = NJson.LocalConfigFile("TA.app.ui.config.json");
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private TAUIConfigManager() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~TAUIConfigManager()
+        {
+            Shutdown();
+        }
+
+        #endregion
+
+        #region Override Methods and Properties
+
+        /// <summary>
+        /// Gets Config File Name.
+        /// </summary>
+        public override string FileName { get { return _fileName; } }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets AppInfo Config.
+        /// </summary>
+        public StatusBarConfig AppInfo
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.AppInfo : null;
+            }
+        }
+        /// <summary>
+        /// Gets ClientInfo Config.
+        /// </summary>
+        public StatusBarConfig ClientInfo
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.ClientInfo : null;
+            }
+        }
+        /// <summary>
+        /// Gets LocalDb Config.
+        /// </summary>
+        public StatusBarConfig LocalDb
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.LocalDb : null;
+            }
+        }
+        /// <summary>
+        /// Gets SCW Config.
+        /// </summary>
+        public StatusBarConfig SCW
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.SCW : null;
+            }
+        }
+        /// <summary>
+        /// Gets TAServer Config.
+        /// </summary>
+        public StatusBarConfig TAServer
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.TAServer : null;
+            }
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ 09.App/DMT.TOD.App/Services/UIConfigs.cs
@@ -0,0 +1,182 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace DMT.Configurations
+{
+    #region TODUIConfig
+
+    /// <summary>
+    /// The TODUIConfig class.
+    /// </summary>
+    [JsonObject(MemberSerialization.OptOut)]
+    public class TODUIConfig
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TODUIConfig()
+        {
+            this.AppInfo = new StatusBarConfig() { Name = "AppInfo", Visible = true };
+            this.ClientInfo = new StatusBarConfig() { Name = "ClientInfo", Visible = true };
+            this.LocalDb = new StatusBarConfig() { Name = "LocalDb", Visible = false };
+            this.SCW = new StatusBarConfig() { Name = "SCW", Visible = false };
+            this.TAServer = new StatusBarConfig() { Name = "TAServer", Visible = true };
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets or sets AppInfo status bar.</summary>
+        public StatusBarConfig AppInfo { get; set; }
+        /// <summary>Gets or sets ClientInfo status bar.</summary>
+        public StatusBarConfig ClientInfo { get; set; }
+        /// <summary>Gets or sets LocalDb status bar.</summary>
+        public StatusBarConfig LocalDb { get; set; }
+        /// <summary>Gets or sets SCW status bar.</summary>
+        public StatusBarConfig SCW { get; set; }
+        /// <summary>Gets or sets TAServer status bar.</summary>
+        public StatusBarConfig TAServer { get; set; }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region TODUIConfigManager
+
+    /// <summary>
+    /// TOD UI Config Manager class.
+    /// </summary>
+    public class TODUIConfigManager : JsonConfigFileManger<TODUIConfig>
+    {
+        #region Static Instance Access
+
+        private static TODUIConfigManager _instance = null;
+
+        /// <summary>
+        /// Gets ConfigManager instance access.
+        /// </summary>
+        public static TODUIConfigManager Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(TODUIConfigManager))
+                    {
+                        _instance = new TODUIConfigManager();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private string _fileName = NJson.LocalConfigFile("TOD.app.ui.config.json");
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private TODUIConfigManager() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~TODUIConfigManager()
+        {
+            Shutdown();
+        }
+
+        #endregion
+
+        #region Override Methods and Properties
+
+        /// <summary>
+        /// Gets Config File Name.
+        /// </summary>
+        public override string FileName { get { return _fileName; } }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets AppInfo Config.
+        /// </summary>
+        public StatusBarConfig AppInfo
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.AppInfo : null;
+            }
+        }
+        /// <summary>
+        /// Gets ClientInfo Config.
+        /// </summary>
+        public StatusBarConfig ClientInfo
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.ClientInfo : null;
+            }
+        }
+        /// <summary>
+        /// Gets LocalDb Config.
+        /// </summary>
+        public StatusBarConfig LocalDb
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.LocalDb : null;
+            }
+        }
+        /// <summary>
+        /// Gets SCW Config.
+        /// </summary>
+        public StatusBarConfig SCW
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.SCW : null;
+            }
+        }
+        /// <summary>
+        /// Gets TAServer Config.
+        /// </summary>
+        public StatusBarConfig TAServer
+        {
+            get
+            {
+                if (null == Value) LoadConfig();
+                return (null != Value) ? Value.TAServer : null;
+            }
+        }
+
+        #endregion
+    }
+
+    #endregion
+}

e1ed44bcaa0275e1798e81106d68f40bf7eb1610
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-19 16:58:13 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #928 - DMT.TAxTODv3   - DMT.TOD.Models project.     - Implements BOS Window (Collector) #1

--- 09.App/DMT.TOD.App/TOD/Windows/UserShifts/BOSWindow.xaml.cs
+++ 09.App/DMT.TOD.App/TOD/Windows/UserShifts/BOSWindow.xaml.cs
@@ -31,6 +31,13 @@ namespace DMT.TOD.Windows.UserShifts
 
         #endregion
 
+        #region Internal Variables
+
+        private User _user = null;
+        private TSB _tsb = null;
+
+        #endregion
+
         #region Button Handlers
 
         private void cmdCancel_Click(object sender, RoutedEventArgs e)
@@ -40,7 +47,61 @@ namespace DMT.TOD.Windows.UserShifts
 
         private void cmdOk_Click(object sender, RoutedEventArgs e)
         {
+            if (cbShift.SelectedIndex == -1)
+            {
+                cbShift.Focus();
+                return;
+            }
+            Models.Shift shift = cbShift.SelectedItem as Models.Shift;
+            if (null != shift)
+            {
+                UserShift inst = UserShift.Create(shift, _user).Value();
+                if (null != inst)
+                {
+                    var success = UserShift.BeginUserShift(inst).Ok;
+                    if (!success)
+                    {
+                        /*
+                        DMT.Windows.MessageBoxWindow msg = new DMT.Windows.MessageBoxWindow();
+                        msg.Owner = Application.Current.MainWindow;
+                        msg.Setup("ไม่สามารถเปิดกะใหม่ได้ เนื่องจาก ยังมีกะที่ยังไม่ป้อนรายได้", "DMT - Tour of Duty");
+                        msg.ShowDialog();
+                        */
+                    }
+                }
+            }
+
             DialogResult = true;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        /// <param name="user">The Collector User.</param>
+        public void Setup(User user)
+        {
+            _user = user;
+            if (null != _user)
+            {
+                DateTime dt = DateTime.Now;
+                var shifts = Models.Shift.GetShifts().Value();
+                cbShift.ItemsSource = shifts;
+
+                _tsb = TSB.GetCurrent().Value();
+                if (null != _tsb)
+                {
+                    txtPlaza.Text = _tsb.TSBNameTH;
+                }
+                txtDate.Text = dt.ToThaiDateString();
+                txtTime.Text = dt.ToThaiTimeString();
+
+                txtID.Text = _user.UserId;
+                txtName.Text = _user.FullNameTH;
+            }
         }
 
         #endregion

0d3046b6c9f23411d137d2fae026eac61ac0f951
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-19 16:10:38 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #926 - DMT.TAxTODv3   - DMT.TOD.Models project.     - Implements ChangeShift (TSB) #1

---
+++ 09.App/DMT.TOD.App/Models/Lane.Models.cs
@@ -0,0 +1,247 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using DMT.Configurations;
+using DMT.Services;
+using DMT.Models;
+
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region UserCache
+
+    /// <summary>
+    /// The User Cache class.
+    /// </summary>
+    public class UserCache
+    {
+        #region Internal Variables
+
+        private Dictionary<string, User> _users = null;
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public UserCache() : base()
+        {
+            _users = new Dictionary<string, User>();
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~UserCache()
+        {
+            if (null != _users)
+            {
+                _users.Clear();
+            }
+            _users = null;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Clear.
+        /// </summary>
+        public void Clear()
+        {
+            if (null == _users) return;
+            _users.Clear();
+        }
+        /// <summary>
+        /// Check is userid is in cache.
+        /// </summary>
+        /// <param name="userId">The User Id.</param>
+        /// <returns>Returns true if user Id is exist in cache.</returns>
+        public bool Contains(string userId)
+        {
+            return _users.ContainsKey(userId);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Indexer access User in cache by UserId.
+        /// </summary>
+        /// <param name="userId">The User Id.</param>
+        /// <returns>Returns match User by specificed parameter.</returns>
+        public User this[string userId]
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(userId)) return null;
+
+                if (!_users.ContainsKey(userId))
+                {
+                    var usr = User.GetByUserId(userId).Value();
+                    if (null == usr) return null;
+                    // add to cache.
+                    _users.Add(userId, usr);
+                }
+
+                return _users[userId];
+            }
+            set { }
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region LaneJob
+
+    /// <summary>
+    /// The LaneJob class.
+    /// </summary>
+    public class LaneJob
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private LaneJob() { }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="job">The SCWJob instance.</param>
+        /// <param name="user">The User instance.</param>
+        public LaneJob(SCWJob job, User user)
+        {
+            this.Job = job;
+            this.User = user;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Public Properties
+
+        #region Model instance(s)
+
+        /// <summary>Gets User.</summary>
+        public User User { get; private set; }
+
+        /// <summary>Gets LaneJob.</summary>
+        public SCWJob Job { get; private set; }
+
+        #endregion
+
+        #region User
+
+        /// <summary>Gets User Id.</summary>
+        public string UserId
+        {
+            get { return (null != User) ? User.UserId : string.Empty; }
+            set { }
+        }
+        /// <summary>Gets User Full Name EN.</summary>
+        public string FullNameEN
+        {
+            get { return (null != User) ? User.FullNameEN : string.Empty; }
+            set { }
+        }
+        /// <summary>Gets User Full Name TH.</summary>
+        public string FullNameTH
+        {
+            get { return (null != User) ? User.FullNameTH : string.Empty; }
+            set { }
+        }
+
+        #endregion
+
+        #region LaneJob
+
+        /// <summary>Gets Job No.</summary>
+        public int? JobNo
+        {
+            get { return (null != Job) ? Job.jobNo : null; }
+            set { }
+        }
+
+        /// <summary>Gets Begin Job DateTime.</summary>
+        public DateTime? Begin
+        {
+            get { return (null != Job) ? Job.bojDateTime : null; }
+            set { }
+        }
+        /// <summary>Gets Begin Job Date in string.</summary>
+        public string BeginDateString
+        {
+            get
+            {
+                string val = (null != Job && Job.bojDateTime.HasValue) ?
+                    Job.bojDateTime.Value.ToThaiDateTimeString("dd/MM/yyyy") : string.Empty;
+                return val;
+            }
+            set { }
+        }
+        /// <summary>Gets Begin Job Time in string.</summary>
+        public string BeginTimeString
+        {
+            get
+            {
+                string val = (null != Job && Job.bojDateTime.HasValue) ?
+                    Job.bojDateTime.Value.ToThaiTimeString() : string.Empty;
+                return val;
+            }
+            set { }
+        }
+
+        /// <summary>Gets End Job DateTime.</summary>
+        public DateTime? End
+        {
+            get { return (null != Job) ? Job.eojDateTime : null; }
+            set { }
+        }
+        /// <summary>Gets End Job Date in string.</summary>
+        public string EndDateString
+        {
+            get
+            {
+                string val = (null != Job && Job.eojDateTime.HasValue) ?
+                    Job.eojDateTime.Value.ToThaiDateTimeString("dd/MM/yyyy") : string.Empty;
+                return val;
+            }
+            set { }
+        }
+        /// <summary>Gets End Job Time in string.</summary>
+        public string EndTimeString
+        {
+            get
+            {
+                string val = (null != Job && Job.eojDateTime.HasValue) ?
+                    Job.eojDateTime.Value.ToThaiTimeString() : string.Empty;
+                return val;
+            }
+            set { }
+        }
+
+        /// <summary>Check Has Job.</summary>
+        public bool HasJob { get { return null != Job; } set { } }
+
+        #endregion
+
+        #endregion
+    }
+
+    #endregion
+}
--- 09.App/DMT.TOD.App/TOD/Pages/TollAdmin/ChangeShiftPage.xaml.cs
+++ 09.App/DMT.TOD.App/TOD/Pages/TollAdmin/ChangeShiftPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
+using DMT.Configurations;
 using DMT.Models;
 using DMT.Services;
+
 using NLib.Services;
 using NLib.Reflection;
 
@@ -14,6 +16,8 @@ using NLib.Reflection;
 
 namespace DMT.TOD.Pages.TollAdmin
 {
+    using scwOps = Services.Operations.SCW.TOD;
+
     /// <summary>
     /// Interaction logic for ChangeShiftPage.xaml
     /// </summary>
@@ -28,6 +32,16 @@ namespace DMT.TOD.Pages.TollAdmin
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Internal Variables
+
+        private User _user = null;
+        private List<User> _users = null;
+        private TSB _tsb = null;
+        private List<Plaza> _plazas = null;
+        private List<LaneJob> _jobs = null;
 
         #endregion
 
@@ -54,17 +68,83 @@ namespace DMT.TOD.Pages.TollAdmin
             PageContentManager.Instance.Current = page;
         }
 
+        private void RefreshLanes()
+        {
+            lvJobs.ItemsSource = null;
+            if (null == _tsb || null == _plazas || _plazas.Count <= 0 || null == _user) return;
+
+            // Read all scw jobs.
+            int networkId = TODConfigManager.Instance.DMT.networkId;
+            
+            if (null == _jobs)
+            {
+                // Create new job list.
+                _jobs = new List<LaneJob>();
+            }
+            _jobs.Clear();
+
+            // Gets jobs from each plaza.
+            _plazas.ForEach(plaza => 
+            {
+                if (null != _users && _users.Count > 0)
+                {
+                    _users.ForEach(usr => 
+                    {
+                        var param = new SCWJobList();
+                        param.networkId = networkId;
+                        param.plazaId = plaza.SCWPlazaId;
+                        param.staffId = usr.UserId;
+
+                        var ret = scwOps.jobList(param);
+                        if (null != ret && null != ret.list && ret.list.Count > 0)
+                        {
+                            ret.list.ForEach(job =>
+                            {
+                                _jobs.Add(new LaneJob(job, usr));
+                            });
+                        }
+                    });
+                }
+            });
+
+            lvJobs.ItemsSource = _jobs;
+        }
+
         #endregion
 
         #region Public Methods
 
         public void Setup(User user)
         {
-            List<SCWJob> jobs = new List<SCWJob>();
-            jobs.Add(new SCWJob() { staffId = "00111", laneId = 1 });
-            jobs.Add(new SCWJob() { staffId = "00111", laneId = 2 });
-            jobs.Add(new SCWJob() { staffId = "00111", laneId = 3 });
-            lvJobs.ItemsSource = jobs;
+            _user = user;
+            if (null != _user)
+            {
+                DateTime dt = DateTime.Now;
+                var shifts = Models.Shift.GetShifts().Value();
+                cbShifts.ItemsSource = shifts;
+
+                _tsb = TSB.GetCurrent().Value();
+                if (null == _tsb) return;
+                _plazas = Plaza.GetTSBPlazas(_tsb.TSBId).Value();
+
+                _users = new List<User>();
+                var userShifts = UserShift.GetUnCloseUserShifts().Value();
+                if (null != userShifts && userShifts.Count > 0)
+                {
+                    UserCache cache = new UserCache();
+                    userShifts.ForEach(userShift => 
+                    {
+                        if (!cache.Contains(userShift.UserId))
+                        {
+                            var usr = cache[userShift.UserId];
+                            if (null != usr) _users.Add(usr);
+                        }
+                    });
+                }
+
+                // Load related lane data.
+                RefreshLanes();
+            }
         }
 
         #endregion

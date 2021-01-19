da9f48672b1d68f416d9a81a5c0c3d165b459e96
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-20 04:47:29 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #965 - DMT.TAxTODv3   - DMT.Account.App project.     - Update UserSearchManager.cs   - DMT.TA.App project.     - Update UserSearchManager.cs   - DMT.TOD.App project.     - Update UserSearchManager.cs

--- 09.App/DMT.Account.App/SignIn/UserSearchManager.cs
+++ 09.App/DMT.Account.App/SignIn/UserSearchManager.cs
@@ -1,12 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Using
 
-namespace DMT.SignIn.Common
+using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls
 {
-    class UserSearchManager
+    /// <summary>
+    /// The User Search Manager helper controls.
+    /// </summary>
+    public class UserSearchManager
     {
+        #region Singelton
+
+        private static UserSearchManager _instance = null;
+        /// <summary>
+        /// Singelton Access.
+        /// </summary>
+        public static UserSearchManager Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (typeof(UserSearchManager))
+                    {
+                        _instance = new UserSearchManager();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private UserSearchManager() : base() { }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~UserSearchManager() { }
+
+        #endregion
+
+        #region Public Methods and Properties
+
+        /// <summary>
+        /// Select User.
+        /// </summary>
+        /// <param name="userId">The User Id.</param>
+        /// <param name="roles">The Roles.</param>
+        /// <returns></returns>
+        public User SelectUser(string userId, params string[] roles)
+        {
+            User ret = null;
+
+            var users = User.FilterByUserId(userId, roles).Value();
+            if (null != users)
+            {
+                if (users.Count == 1)
+                {
+                    ret = users[0];
+                }
+                else if (users.Count > 1)
+                {
+                    var win = AccountApp.Windows.UserSearch;
+                    // change title.
+                    if (!string.IsNullOrEmpty(this.Title)) win.Title = this.Title;
+                    // setup user list for selection.
+                    win.Setup(users);
+                    if (win.ShowDialog() == false || null == win.SelectedUser)
+                    {
+                        // No user selected.
+                        ret = null;
+                    }
+                    else
+                    {
+                        // User selected.
+                        ret = win.SelectedUser;
+                    }
+                }
+            }
+            return ret;
+        }
+        /// <summary>
+        /// Gets or sets Popup window title.
+        /// </summary>
+        public string Title { get; set; }
+
+        #endregion
     }
 }

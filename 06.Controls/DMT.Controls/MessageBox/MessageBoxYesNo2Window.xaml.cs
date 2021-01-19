24c6ca9513df760b1508f574f858a12dfa6128c0
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-20 00:06:17 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #943 - DMT.TAxTODv3   - DMT.Controls Project.     - Refactor MessageBoxYesNo2Window UI layout.     - Refactor MessageBoxYesNo3Window UI layout.

---
+++ 06.Controls/DMT.Controls/MessageBox/MessageBoxYesNo2Window.xaml.cs
@@ -0,0 +1,74 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+using System.Windows.Media;
+
+#endregion
+
+namespace DMT.Windows
+{
+    /// <summary>
+    /// Interaction logic for MessageBoxYesNo2Window.xaml
+    /// </summary>
+    public partial class MessageBoxYesNo2Window : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MessageBoxYesNo2Window()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Buton Handlers
+
+        private void cmdOK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        /// <param name="msg1"></param>
+        /// <param name="msg2"></param>
+        /// <param name="msg3"></param>
+        /// <param name="msg4"></param>
+        /// <param name="msg5"></param>
+        /// <param name="head"></param>
+        public void Setup(string msg1, string msg2, string msg3, string msg4, string msg5, string head)
+        {
+            this.Title = head;
+            txtMsg1.Text = msg1;
+            txtMsg2.Text = msg2;
+            txtMsg3.Text = msg3;
+            txtMsg4.Text = msg4;
+            txtMsg5.Text = msg5;
+
+            txtMsg1.Foreground = new SolidColorBrush(Colors.Black);
+            txtMsg2.Foreground = new SolidColorBrush(Colors.Red);
+            txtMsg3.Foreground = new SolidColorBrush(Colors.Black);
+            txtMsg4.Foreground = new SolidColorBrush(Colors.Red);
+            txtMsg5.Foreground = new SolidColorBrush(Colors.Black);
+
+        }
+    }
+}

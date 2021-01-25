4f6e5bb9bda3e3c926822c6a6fc3ae43a4938a9f
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-25 14:33:30 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1076 - DMT.TAxTODv3   - DMT.TA.App     - Update TSB Credit UI #11.

---
+++ 09.App/DMT.TA.App/Services/TAManagers.cs
@@ -0,0 +1,32 @@
+﻿#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+//using System.Windows.Forms;
+//using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Reflection;
+
+using DMT.Configurations;
+using DMT.Controls;
+using DMT.Services;
+using DMT.Models;
+using DMT.Models.ExtensionMethods;
+
+using NLib;
+using NLib.IO;
+using NLib.Services;
+using NLib.Reports.Rdlc;
+using NLib.Reflection;
+
+using RestSharp;
+
+#endregion
+
+namespace DMT.Services
+{
+
+}

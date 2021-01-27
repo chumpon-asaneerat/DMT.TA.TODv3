fd3a579f6e4cd1cd6b5e568aa29a867ca35b86fa
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-27 23:48:52 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1152 - DMT.TAxTODv3   - DMT.TOD.App     - Update RevenueSummarySearchWindow #4.     - Update RevenueSlipSearchWindow #4.

--- 09.App/DMT.TOD.App/TOD/Windows/Reports/RevenueSummarySearchWindow.xaml.cs
+++ 09.App/DMT.TOD.App/TOD/Windows/Reports/RevenueSummarySearchWindow.xaml.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
+using DMT.Models;
+using DMT.Services;
+using NLib.Services;
+using NLib.Reflection;
+using System.Windows.Threading;
+
+#endregion
 
 namespace DMT.TOD.Windows.Reports
 {
@@ -19,9 +21,36 @@ namespace DMT.TOD.Windows.Reports
     /// </summary>
     public partial class RevenueSummarySearchWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public RevenueSummarySearchWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        #endregion
     }
 }

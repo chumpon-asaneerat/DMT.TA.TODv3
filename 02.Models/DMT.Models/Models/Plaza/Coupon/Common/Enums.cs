c2451686e457316b770d7a21a6c7d1749dfe5022
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-29 00:39:35 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1173 - DMT.TAxTODv3   - DMT.Models project.     - Add Enums.cs -> CouponType     - Add TSBCouponBalance.cs     - Add TSBCouponSummary.cs     - Add TSBCouponTransaction.cs

---
+++ 02.Models/DMT.Models/Models/Plaza/Coupon/Common/Enums.cs
@@ -0,0 +1,41 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using NLib;
+using NLib.Design;
+using NLib.Reflection;
+
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+using SQLiteNetExtensions.Extensions;
+// required for JsonIgnore attribute.
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+
+#endregion
+
+namespace DMT.Models
+{
+    #region CouponType
+
+    /// <summary>
+    /// The Coupon Type enum.
+    /// </summary>
+    public enum CouponType : int
+    {
+        /// <summary>
+        /// Coupon 35 BHT
+        /// </summary>
+        BHT35 = 35,
+        /// <summary>
+        /// Coupon 80 BHT
+        /// </summary>
+        BHT80 = 80
+    }
+
+    #endregion
+}

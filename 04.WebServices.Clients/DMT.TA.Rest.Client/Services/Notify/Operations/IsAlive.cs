2e359d0e90d56cdbfa882a862d88f0ab7c8b17f8
chumpon-asaneerat <chumponsenate@gmail.com>
2021-01-29 01:21:13 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1176 - DMT.TAxTODv3   - DMT.TA.Rest.Client project.     - Implements Notify->IsAlive Operation   - DMT.TOD.Rest.Client project.     - Implements Notify->IsAlive Operation

---
+++ 04.WebServices.Clients/DMT.TA.Rest.Client/Services/Notify/Operations/IsAlive.cs
@@ -0,0 +1,26 @@
+﻿#region Usings
+
+using System.Collections.Generic;
+using DMT.Configurations;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Services.Operations
+{
+    static partial class TA
+    {
+        static partial class Notify
+        {
+            /// <summary>
+            /// IsAlive.
+            /// </summary>
+            /// <returns>Returns NRestResult instance.</returns>
+            public static NRestResult IsAlive()
+            {
+                var ret = Execute(RouteConsts.TA.Notify.IsAlive.Url);
+                return ret;
+            }
+        }
+    }
+}
---
+++ 04.WebServices.Clients/DMT.TOD.Rest.Client/Services/Notify/Operations/IsAlive.cs
@@ -0,0 +1,25 @@
+﻿#region Usings
+
+using System.Collections.Generic;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Services.Operations
+{
+    static partial class TOD
+    {
+        static partial class Notify
+        {
+            /// <summary>
+            /// IsAlive.
+            /// </summary>
+            /// <returns>Returns NRestResult instance.</returns>
+            public static NRestResult IsAlive()
+            {
+                var ret = Execute(RouteConsts.TOD.Notify.IsAlive.Url);
+                return ret;
+            }
+        }
+    }
+}

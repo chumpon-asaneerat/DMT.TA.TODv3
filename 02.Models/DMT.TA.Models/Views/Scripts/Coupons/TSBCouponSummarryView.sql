20517c823d65125079c9b17b31a716fb4322230d
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-03 18:57:26 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1303 - DMT.TAxTODv3   - DMT.TA.Models project     - Update TSBCouponSummarryView.sql #1.

--- 02.Models/DMT.TA.Models/Views/Scripts/Coupons/TSBCouponSummarryView.sql
+++ 02.Models/DMT.TA.Models/Views/Scripts/Coupons/TSBCouponSummarryView.sql
@@ -6,7 +6,7 @@ AS
     -- 4 = SoldByTSB
     SELECT A.UserId, A.FullNameEN, A.FullNameTH
     	 --, A.UserReceiveDate
-    	 , (SELECT COUNT(C35.CouponType) 
+    	 , IFNULL((SELECT COUNT(C35.CouponType) 
     	      FROM TSBCouponTransactionView C35 
     		 WHERE C35.CouponType = 35 
     		   AND C35.TSBId = A.TSBId
@@ -15,8 +15,8 @@ AS
                AND (C35.TransactionType = 2 OR C35.TransactionType = 3) -- Lane or SoldByLane
              GROUP BY C35.TSBId
                     , C35.UserId
-    	   ) AS CountCouponBHT35
-    	 , (SELECT SUM(C35.Price) 
+    	   ), 0) AS CountCouponBHT35
+    	 , IFNULL((SELECT SUM(C35.Price) 
     	      FROM TSBCouponTransactionView C35 
     		 WHERE C35.CouponType = 35 
     		   AND C35.TSBId = A.TSBId
@@ -25,8 +25,8 @@ AS
                AND (C35.TransactionType = 2 OR C35.TransactionType = 3) -- Lane or SoldByLane
              GROUP BY C35.TSBId
                     , C35.UserId
-    	   ) AS AmountCouponBHT35
-    	 , (SELECT COUNT(C80.CouponType) 
+    	   ), 0) AS AmountCouponBHT35
+    	 , IFNULL((SELECT COUNT(C80.CouponType)
     	      FROM TSBCouponTransactionView C80
     		 WHERE C80.CouponType = 80
     		   AND C80.TSBId = A.TSBId
@@ -35,8 +35,8 @@ AS
                AND (C80.TransactionType = 2 OR C80.TransactionType = 3) -- Lane or SoldByLane
              GROUP BY C80.TSBId
                     , C80.UserId
-    	   ) AS CountCouponBHT80
-    	 , (SELECT SUM(C80.Price) 
+    	   ), 0)  AS CountCouponBHT80
+    	 , IFNULL((SELECT SUM(C80.Price) 
     	      FROM TSBCouponTransactionView C80
     		 WHERE C80.CouponType = 80
     		   AND C80.TSBId = A.TSBId
@@ -45,13 +45,13 @@ AS
                AND (C80.TransactionType = 2 OR C80.TransactionType = 3) -- Lane or SoldByLane
              GROUP BY C80.TSBId
                     , C80.UserId
-    	   ) AS AmountCouponBHT80
+    	   ), 0) AS AmountCouponBHT80
          , A.TSBId, A.TSBNameEN, A.TSBNameTH
     	 --, A.SoldBy, A.SoldByFullNameEN, A.SoldByFullNameTH
       FROM TSBCouponTransactionView A
      WHERE (    A.UserId IS NOT NULL
             AND A.UserId <> '')
-       AND A.FinishFlag = 1 -- Avaliable.
+       --AND A.FinishFlag = 1 -- Avaliable.
      GROUP BY A.TSBId
             , A.UserId
             --, A.UserReceiveDate

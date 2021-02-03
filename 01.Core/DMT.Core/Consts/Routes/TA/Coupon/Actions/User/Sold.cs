67fdfed03863a605e46a52c89ce42155b6d47c2d
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-03 18:01:17 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1298 - DMT.TAxTODv3   - DMT.Core project.     - Update TA->Coupon->User route.       - Remove Current       - Add Sold

--- 01.Core/DMT.Core/Consts/Routes/TA/Coupon/Actions/User/Sold.cs
+++ 01.Core/DMT.Core/Consts/Routes/TA/Coupon/Actions/User/Sold.cs
@@ -8,11 +8,11 @@
             {
                 static partial class User
                 {
-                    /// <summary>The Gets User Coupon Balance action.</summary>
-                    public static class Current
+                    /// <summary>The Gets User Coupon Sold Balance action.</summary>
+                    public static class Sold
                     {
                         /// <summary>Gets route name.</summary>
-                        public const string Name = "Current";
+                        public const string Name = "Sold";
                         /// <summary>Gets route url.</summary>
                         public const string Url = TSB.Url + @"/" + Name;
                     }

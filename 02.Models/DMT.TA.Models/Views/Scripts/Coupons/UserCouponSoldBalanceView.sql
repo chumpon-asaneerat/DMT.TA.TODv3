8056ded30511aafa7794877664a558d1a5169380
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-04 12:12:29 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1308 - DMT.TAxTODv3   - DMT.TA.Models project     - Update UserCouponSoldBalanceView.sql #1

--- 02.Models/DMT.TA.Models/Views/Scripts/Coupons/UserCouponSoldBalanceView.sql
+++ 02.Models/DMT.TA.Models/Views/Scripts/Coupons/UserCouponSoldBalanceView.sql
@@ -1,79 +1,49 @@
 ﻿CREATE VIEW UserCouponSoldBalanceView
 AS
+	-- Stock = 1, Lane = 2, SoldByLane = 3, SoldByTSB = 4
 	SELECT TSB.TSBId, TSB.TSBNameEN, TSB.TSBNameTH
 		 , (
 		     -- Count No of Coupon 35
-			 SELECT (stock.CouponBHT35 + 
-			         lane.CouponBHT35 - 
-					 tsbsold.CouponBHT35 - 
-					 usrsold.CouponBHT35) AS CouponBHT35
-			   FROM TSBCouponStockBalanceView stock
-			      , TSBCouponLaneBalanceView lane 
-				  , TSBCouponSoldByTSBBalanceView tsbsold
-				  , TSBCouponSoldByLaneBalanceView usrsold
-			  WHERE stock.TSBId = TSB.TSBId
-			    AND lane.TSBId = TSB.TSBId
-			    AND tsbsold.TSBId = TSB.TSBId
-			    AND usrsold.TSBId = TSB.TSBId
+			 SELECT IFNULL(COUNT(*), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE TSBCouponTransaction.TransactionType = 3 -- SoldByLane = 3
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransaction.CouponType = 35
+				AND TSBCouponTransaction.FinishFlag = 0 -- Completed
 		   ) AS CouponBHT35
 		 , (
 		     -- Count No of Coupon 80
-			 SELECT (stock.CouponBHT80 + 
-			         lane.CouponBHT80 - 
-					 tsbsold.CouponBHT80 - 
-					 usrsold.CouponBHT80) AS CouponBHT80
-			   FROM TSBCouponStockBalanceView stock
-			      , TSBCouponLaneBalanceView lane 
-				  , TSBCouponSoldByTSBBalanceView tsbsold
-				  , TSBCouponSoldByLaneBalanceView usrsold
-			  WHERE stock.TSBId = TSB.TSBId
-			    AND lane.TSBId = TSB.TSBId
-			    AND tsbsold.TSBId = TSB.TSBId
-			    AND usrsold.TSBId = TSB.TSBId
+			 SELECT IFNULL(COUNT(*), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE TSBCouponTransaction.TransactionType = 3 -- SoldByLane = 3
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransaction.CouponType = 80
+				AND TSBCouponTransaction.FinishFlag = 0 -- Completed
 		   ) AS CouponBHT80
 		 , (
 		     -- Calc Price of Coupon 35
-			 SELECT (stock.PriceBHT35 + 
-			         lane.PriceBHT35 - 
-					 tsbsold.PriceBHT35 - 
-					 usrsold.PriceBHT35) AS PriceBHT35
-			   FROM TSBCouponStockBalanceView stock
-			      , TSBCouponLaneBalanceView lane 
-				  , TSBCouponSoldByTSBBalanceView tsbsold
-				  , TSBCouponSoldByLaneBalanceView usrsold
-			  WHERE stock.TSBId = TSB.TSBId
-			    AND lane.TSBId = TSB.TSBId
-			    AND tsbsold.TSBId = TSB.TSBId
-			    AND usrsold.TSBId = TSB.TSBId
+			 SELECT IFNULL(SUM(Price), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE TSBCouponTransaction.TransactionType = 3 -- SoldByLane = 3
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransaction.CouponType = 35
+				AND TSBCouponTransaction.FinishFlag = 0 -- Completed
 		   ) AS PriceBHT35
 		 , (
 		     -- Calc Price of Coupon 80
-			 SELECT (stock.PriceBHT80 + 
-			         lane.PriceBHT80 - 
-					 tsbsold.PriceBHT80 - 
-					 usrsold.PriceBHT80) AS PriceBHT80
-			   FROM TSBCouponStockBalanceView stock
-			      , TSBCouponLaneBalanceView lane 
-				  , TSBCouponSoldByTSBBalanceView tsbsold
-				  , TSBCouponSoldByLaneBalanceView usrsold
-			  WHERE stock.TSBId = TSB.TSBId
-			    AND lane.TSBId = TSB.TSBId
-			    AND tsbsold.TSBId = TSB.TSBId
-			    AND usrsold.TSBId = TSB.TSBId
+			 SELECT IFNULL(SUM(Price), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE TSBCouponTransaction.TransactionType = 3 -- SoldByLane = 3
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransaction.CouponType = 80
+				AND TSBCouponTransaction.FinishFlag = 0 -- Completed
 		   ) AS PriceBHT80
 		 , (
 		     -- Calc Price of Coupon all types
-			 SELECT (stock.CouponBHTTotal + 
-			         lane.CouponBHTTotal - 
-					 tsbsold.CouponBHTTotal - 
-					 usrsold.CouponBHTTotal) AS CouponBHTTotal
-			   FROM TSBCouponStockBalanceView stock
-			      , TSBCouponLaneBalanceView lane 
-				  , TSBCouponSoldByTSBBalanceView tsbsold
-				  , TSBCouponSoldByLaneBalanceView usrsold
-			  WHERE stock.TSBId = TSB.TSBId
-			    AND lane.TSBId = TSB.TSBId
-			    AND tsbsold.TSBId = TSB.TSBId
-			    AND usrsold.TSBId = TSB.TSBId
+			 SELECT IFNULL(SUM(Price), 0) 
+			   FROM TSBCouponTransaction 
+			  WHERE TSBCouponTransaction.TransactionType = 3 -- SoldByLane = 3
+				AND TSBCouponTransaction.TSBId = TSB.TSBId
+				AND TSBCouponTransaction.FinishFlag = 0 -- Completed
 		   ) AS CouponBHTTotal
 	  FROM TSB

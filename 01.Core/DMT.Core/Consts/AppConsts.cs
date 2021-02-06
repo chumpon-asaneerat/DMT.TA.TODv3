b9cd03abf88f382884943cac37c00f36818c9ee9
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-07 01:04:43 +0700

    Ported DMTv2 to DMT.TA.TODv1, DMT.TA.TODv2 #1419 - DMT.TAxTODv4   - DMT.Core project     - Update app version.

--- 01.Core/DMT.Core/Consts/AppConsts.cs
+++ 01.Core/DMT.Core/Consts/AppConsts.cs
@@ -11,8 +11,8 @@ namespace DMT
         // common properties
         public static string Version = "1";
         public static string Minor = "4";
-        public static string Build = "7820";
-        public static DateTime LastUpdate = new DateTime(2021, 02, 05, 11, 30, 00);
+        public static string Build = "7950";
+        public static DateTime LastUpdate = new DateTime(2021, 02, 07, 23, 30, 00);
 
         public static class Application
         {

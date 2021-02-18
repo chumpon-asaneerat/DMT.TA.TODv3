84d8221d40d2bfef63bd2cbb6d90e65307642df8
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-19 03:29:07 +0700

    Refactor DMT.TA.TODv4 #1763 - DMT.TAxTODv3   - Implements GetCouponSold for RevenueEntry(TOD) #6.     - Update TSBCouponTransactionView.sql       - Add join LaneView to get PlazaGroupId.

--- 02.Models/DMT.TA.Models/Views/Scripts/Coupons/TSBCouponTransactionView.sql
+++ 02.Models/DMT.TA.Models/Views/Scripts/Coupons/TSBCouponTransactionView.sql
@@ -1,12 +1,14 @@
 ﻿CREATE VIEW TSBCouponTransactionView
 AS
-	SELECT TSBCouponTransaction.* 
-		 , TSB.TSBNameEN
-		 , TSB.TSBNameTH
-		 --, usr.FullNameEN, usr.FullNameTH
-		 --, sup.FullNameEN AS SoldByFullNameEN, sup.FullNameTH AS SoldByFullNameTH
-	  FROM TSBCouponTransaction
-		 , TSB
-		   --LEFT JOIN [UserView] usr ON (TSBCouponTransaction.UserId = usr.UserId) 
-		   --LEFT JOIN [UserView] sup ON (TSBCouponTransaction.SoldBy = sup.UserId) 
-	 WHERE TSBCouponTransaction.TSBId = TSB.TSBId
+    SELECT TSBCouponTransaction.* 
+         , TSB.TSBNameEN
+         , TSB.TSBNameTH
+         --, usr.FullNameEN, usr.FullNameTH
+         --, sup.FullNameEN AS SoldByFullNameEN, sup.FullNameTH AS SoldByFullNameTH
+         , LaneView.PlazaGroupId, LaneView.PlazaGroupNameEN, LaneView.PlazaGroupNameTH    
+      FROM TSBCouponTransaction
+      LEFT JOIN LaneView ON (TSBCouponTransaction.TSBId = LaneView.TSBId AND TSBCouponTransaction.LaneId = LaneView.LaneId)  
+         , TSB
+    --LEFT JOIN [UserView] usr ON (TSBCouponTransaction.UserId = usr.UserId) 
+    --LEFT JOIN [UserView] sup ON (TSBCouponTransaction.SoldBy = sup.UserId) 
+     WHERE TSBCouponTransaction.TSBId = TSB.TSBId

8f4c553615654c024f990acd6b8ef055cf8c615c
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-19 09:48:49 +0700

    Refactor DMT.TA.TODv4 #1788 - DMT.TAxTODv3   - DMT.Models project.     - Implements UserCouponSoldSummary #4.

--- 02.Models/DMT.Models/Models/Plaza/Coupon/User/UserCouponSoldSummary.cs
+++ 02.Models/DMT.Models/Models/Plaza/Coupon/User/UserCouponSoldSummary.cs
@@ -67,6 +67,16 @@ namespace DMT.Models
 
         #region Static Methods
 
+        /// <summary>
+        /// Gets User Coupon Sold Summary.
+        /// </summary>
+        /// <param name="plazaGroup">The target PlazaGroup to get balance.</param>
+        /// <param name="user">The target User to get balance.</param>
+        /// <param name="start">The start of sold date.</param>
+        /// <param name="end">The end of sold date.</param>
+        /// <returns>
+        /// Returns User Coupon Sold Summary. If not found returns null.
+        /// </returns>
         public static NDbResult<UserCouponSoldSummary> GetCouponSoldSummary(PlazaGroup plazaGroup,
             User user, DateTime? start, DateTime? end)
         {
@@ -86,13 +96,67 @@ namespace DMT.Models
             result = GetCouponSoldSummary(tsb, plazaGroup, user, start, end);
             return result;
         }
-
+        /// <summary>
+        /// Gets User Coupon Sold Summary.
+        /// </summary>
+        /// <param name="tsb">The target TSB to get balance.</param>
+        /// <param name="plazaGroup">The target PlazaGroup to get balance.</param>
+        /// <param name="user">The target User to get balance.</param>
+        /// <param name="start">The start of sold date.</param>
+        /// <param name="end">The end of sold date.</param>
+        /// <returns>
+        /// Returns User Coupon Sold Summary. If not found returns null.
+        /// </returns>
         public static NDbResult<UserCouponSoldSummary> GetCouponSoldSummary(TSB tsb, PlazaGroup plazaGroup,
             User user, DateTime? start, DateTime? end)
         {
             var result = new NDbResult<UserCouponSoldSummary>();
+            SQLiteConnection db = Default;
+            if (null == db)
+            {
+                result.DbConenctFailed();
+                return result;
+            }
+            if (null == tsb || null == plazaGroup || null == user)
+            {
+                result.ParameterIsNull();
+                return result;
+            }
 
-            return result;
+            if (!start.HasValue || start.Value == DateTime.MinValue)
+            {
+                // Check Start Date.
+                result.ParameterIsNull();
+                return result;
+            }
+
+            DateTime dt1 = start.Value;
+            DateTime dt2 = (end.HasValue && end.Value != DateTime.MinValue) ? end.Value : DateTime.Now;
+
+            lock (sync)
+            {
+                MethodBase med = MethodBase.GetCurrentMethod();
+                try
+                {
+                    //CouponBHT35
+                    //CouponBHT80
+                    //Price
+
+                    string cmd = @"
+                        SELECT * 
+                          FROM TSBCouponBalanceView
+                         WHERE TSBId = ? ";
+                    var ret = NQuery.Query<FKs>(cmd, tsb.TSBId).FirstOrDefault();
+                    var data = ret.ToModel();
+                    result.Success(data);
+                }
+                catch (Exception ex)
+                {
+                    med.Err(ex);
+                    result.Error(ex);
+                }
+                return result;
+            }
         }
 
         #endregion

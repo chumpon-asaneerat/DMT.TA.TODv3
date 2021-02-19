8f292896294c81cff317757c31f6e70e4c03145c
chumpon-asaneerat <chumponsenate@gmail.com>
2021-02-19 07:45:42 +0700

    Refactor DMT.TA.TODv4 #1783 - DMT.TAxTODv3   - DMT.TA.Models project.     - Implements UserCouponSoldByLaneTransactionView.sql #1.

---
+++ 02.Models/DMT.TA.Models/Views/Scripts/Coupons/UserCouponSoldByLaneTransactionView.sql
@@ -0,0 +1,13 @@
+﻿-- Stock = 1, Lane = 2, SoldByLane = 3, SoldByTSB = 4
+CREATE VIEW UserCouponSoldByLaneTransactionView
+AS
+	SELECT TransactionType, TransactionDate
+	     , CouponId, CouponType, Price
+		 , FinishFlag
+		 , UserId , FullNameEN, FullNameTH
+		 , UserReceiveDate
+		 , SoldBy, SoldByFullNameEN, SoldByFullNameTH
+		 , SoldDate
+	 FROM TSBCouponTransaction 
+	WHERE TransactionType = 3 -- SoldByLane = 3
+	  AND FinishFlag = 0 -- Completed

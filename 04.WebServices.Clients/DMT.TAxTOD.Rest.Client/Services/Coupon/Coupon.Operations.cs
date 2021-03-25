918dda49cc45b10ebd77893bc68404b62efe25d4
chumpon-asaneerat <chumponsenate@gmail.com>
2021-03-25 10:32:11 +0700

    Refactor DMT.TA.TODv4 #2015 - DMT.TAxTODv3   - DMT.TAxTOD.Rest.Client     - Implements TAxTOD Coupon Operation - Inquiry.

--- 04.WebServices.Clients/DMT.TAxTOD.Rest.Client/Services/Coupon/Coupon.Operations.cs
+++ 04.WebServices.Clients/DMT.TAxTOD.Rest.Client/Services/Coupon/Coupon.Operations.cs
@@ -50,6 +50,17 @@ namespace DMT.Services.Operations
                     RouteConsts.TAxTOD.Coupon.Received.Url, new { serialNo = couponSN });
                 return ret;
             }
+            /// <summary>
+            /// Execute Inquiry api.
+            /// </summary>
+            /// <param name="couponSN">The Inquiry seaarch parameter.</param>
+            /// <returns>Returns instance of NRestResult.</returns>
+            public static NRestResult<List<TACouponInquiry>> Inquiry(Search.TAxTOD.Coupon.Inquiry value)
+            {
+                var ret = Execute<List<TACouponInquiry>>(
+                    RouteConsts.TAxTOD.Coupon.Inquiry.Url, value);
+                return ret;
+            }
         }
     }
 }

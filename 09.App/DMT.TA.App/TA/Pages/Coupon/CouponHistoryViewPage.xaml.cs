53ccc8a769f80ef64733312d90fe6c2908440dcb
chumpon-asaneerat <chumponsenate@gmail.com>
2021-03-29 11:38:55 +0700

    Refactor DMT.TA.TODv4 #2025 - DMT.TAxTODv3   - DMT.TA.App project     - Update Coupon History Page.       - Fixed DateTimePicker - Show thai.

--- 09.App/DMT.TA.App/TA/Pages/Coupon/CouponHistoryViewPage.xaml.cs
+++ 09.App/DMT.TA.App/TA/Pages/Coupon/CouponHistoryViewPage.xaml.cs
@@ -4,9 +4,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Threading;
 using System.Reflection;
 
@@ -42,7 +44,31 @@ namespace DMT.TA.Pages.Coupon
 
         #region Internal Variables
 
+        //private CultureInfo culture = new CultureInfo("th-TH") { DateTimeFormat = { Calendar = new ThaiBuddhistCalendar() } };
+        private CultureInfo culture = new CultureInfo("th-TH");
+        private XmlLanguage language = XmlLanguage.GetLanguage("th-TH");
+
         private User _chief = null;
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Setup DateTime Picker
+            // Work From
+            dtWorkDateFrom.CultureInfo = culture;
+            dtWorkDateFrom.Language = language;
+            // Work To
+            dtWorkDateTo.CultureInfo = culture;
+            dtWorkDateTo.Language = language;
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
 
         #endregion
 

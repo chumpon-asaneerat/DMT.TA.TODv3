60336bdb7ab2f8c9d6d47a32990fe841ab3c9d01
chumpon-asaneerat <chumponsenate@gmail.com>
2021-05-05 08:10:11 +0700

    Refactor DMT.TA.TODv4 #2030 - DMT.TAxTODv3   - DMT.Core project     - Update TAxTOD routes #2.

--- 01.Core/DMT.Core/Routes/TAxTOD/TA/TAxTOD.TA.RouteConsts.cs
+++ 01.Core/DMT.Core/Routes/TAxTOD/TA/TAxTOD.TA.RouteConsts.cs
@@ -15,6 +15,25 @@ namespace DMT
         {
             static partial class TACredit
             {
+                // Url: /api/account/tsbcredit/save
+                /// <summary>The TA Credit Low Limit Save Class.</summary>
+                public static partial class Save
+                {
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Save";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = @"/api/ta/creditlowlimit/save";
+                }
+
+                // Url: /api/account/tsbcredit
+                /// <summary>The TA Credit Low Limit Gets (Search) Class.</summary>
+                public static partial class Gets
+                {
+                    /// <summary>Gets route name.</summary>
+                    public const string Name = "Gets";
+                    /// <summary>Gets route url.</summary>
+                    public const string Url = @"/api/ta/creditlowlimit/get";
+                }
             }
         }
     }

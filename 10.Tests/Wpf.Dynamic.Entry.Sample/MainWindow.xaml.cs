cd5a4fc8c2aef64abafe0b353705694b75a3597f
chumpon-asaneerat <chumponsenate@gmail.com>
2021-05-11 10:23:40 +0700

    Refactor DMT.TA.TODv4 #2071 - DMT.TAxTODv3   - Wpf.Dynamic.Entry.Sample project.     - Update code #1.

--- 10.Tests/Wpf.Dynamic.Entry.Sample/MainWindow.xaml.cs
+++ 10.Tests/Wpf.Dynamic.Entry.Sample/MainWindow.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region Using
+
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Reflection;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Text;
+
+using DMT.Models;
+
+#endregion
 
 namespace Wpf.Dynamic.Entry.Sample
 {
@@ -20,9 +19,16 @@ namespace Wpf.Dynamic.Entry.Sample
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
---
+++ 10.Tests/Wpf.Dynamic.Entry.Sample/Models/Models.cs
@@ -0,0 +1,12 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace DMT.Models
+{
+
+}

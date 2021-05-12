c582aa4a159d8a09998081116b08b2eec0df7780
chumpon-asaneerat <chumponsenate@gmail.com>
2021-05-12 10:26:10 +0700

    Refactor DMT.TA.TODv4 #2080 - DMT.TAxTODv3   - DMT.TOD.App project.     - Change Shift Page -> remove TSBId column.   - Upadte app version.

--- 01.Core/DMT.Core/Consts/AppConsts.cs
+++ 01.Core/DMT.Core/Consts/AppConsts.cs
@@ -11,8 +11,8 @@ namespace DMT
         // common properties
         public static string Version = "1";
         public static string Minor = "4";
-        public static string Build = "9665";
-        public static DateTime LastUpdate = new DateTime(2021, 05, 10, 12, 00, 00);
+        public static string Build = "9666";
+        public static DateTime LastUpdate = new DateTime(2021, 05, 12, 12, 00, 00);
 
         public static class Application
         {

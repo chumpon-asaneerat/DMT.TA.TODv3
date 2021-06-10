3e12e7d424e980e1b7426d08deccdee449254197
chumpon-asaneerat <chumponsenate@gmail.com>
2021-06-10 22:11:31 +0700

    Refactor DMT.TA.TODv4 #2144 - DMT.TAxTODv3   - Update App version.

--- 01.Core/DMT.Core/Consts/AppConsts.cs
+++ 01.Core/DMT.Core/Consts/AppConsts.cs
@@ -11,8 +11,8 @@ namespace DMT
         // common properties
         public static string Version = "1";
         public static string Minor = "4";
-        public static string Build = "10035";
-        public static DateTime LastUpdate = new DateTime(2021, 06, 04, 16, 30, 00);
+        public static string Build = "10040";
+        public static DateTime LastUpdate = new DateTime(2021, 06, 10, 22, 10, 00);
 
         public static class Application
         {

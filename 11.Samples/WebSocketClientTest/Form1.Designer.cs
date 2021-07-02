16406130803464926e646ce4f8f74c829a6a76e3
chumpon-asaneerat <chumponsenate@gmail.com>
2021-07-03 04:07:30 +0700

    Refactor DMT.TA.TODv4 #2176 - DMT.TAxTODv3   - Add WebSocketClientTest project.

---
+++ 11.Samples/WebSocketClientTest/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace WebSocketClientTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmdClear2 = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.cmdClear1 = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.cmdSend = new System.Windows.Forms.Button();
+            this.txtRecv = new System.Windows.Forms.TextBox();
+            this.cmdDisconnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmdConnect = new System.Windows.Forms.Button();
+            this.txtLogs = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cmdClear3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(802, 582);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.cmdClear2);
+            this.tabPage1.Controls.Add(this.txtUrl);
+            this.tabPage1.Controls.Add(this.cmdClear1);
+            this.tabPage1.Controls.Add(this.txtSend);
+            this.tabPage1.Controls.Add(this.cmdSend);
+            this.tabPage1.Controls.Add(this.txtRecv);
+            this.tabPage1.Controls.Add(this.cmdDisconnect);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.cmdConnect);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(794, 553);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Client";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtLogs);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(794, 553);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Logs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 24);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "URL: (i.e. ws://127.0.0.1:4000/echo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Send:";
+            // 
+            // cmdClear2
+            // 
+            this.cmdClear2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdClear2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdClear2.Location = new System.Drawing.Point(639, 509);
+            this.cmdClear2.Name = "cmdClear2";
+            this.cmdClear2.Size = new System.Drawing.Size(136, 36);
+            this.cmdClear2.TabIndex = 32;
+            this.cmdClear2.Text = "Clear";
+            this.cmdClear2.UseVisualStyleBackColor = true;
+            this.cmdClear2.Click += new System.EventHandler(this.cmdClear2_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(19, 42);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(614, 28);
+            this.txtUrl.TabIndex = 23;
+            this.txtUrl.Text = "ws://127.0.0.1:4000/echo";
+            // 
+            // cmdClear1
+            // 
+            this.cmdClear1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdClear1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdClear1.Location = new System.Drawing.Point(639, 275);
+            this.cmdClear1.Name = "cmdClear1";
+            this.cmdClear1.Size = new System.Drawing.Size(136, 36);
+            this.cmdClear1.TabIndex = 31;
+            this.cmdClear1.Text = "Clear";
+            this.cmdClear1.UseVisualStyleBackColor = true;
+            this.cmdClear1.Click += new System.EventHandler(this.cmdClear1_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSend.Location = new System.Drawing.Point(19, 112);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(614, 199);
+            this.txtSend.TabIndex = 25;
+            this.txtSend.Text = "{\"jsonrpc\":\"2.0\",\"method\":\"TOD_adjustSizeRequest\",\"staffId\":123456,\"staffName\":\"ส" +
+    "มชาย\",\"staffLastName\":\"เข็มขัด\",\"bojDateTime\":\"2021-05-31T22:55:31.000Z\"}";
+            // 
+            // cmdSend
+            // 
+            this.cmdSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSend.Location = new System.Drawing.Point(639, 112);
+            this.cmdSend.Name = "cmdSend";
+            this.cmdSend.Size = new System.Drawing.Size(136, 36);
+            this.cmdSend.TabIndex = 30;
+            this.cmdSend.Text = "Send";
+            this.cmdSend.UseVisualStyleBackColor = true;
+            this.cmdSend.Click += new System.EventHandler(this.cmdSend_Click);
+            // 
+            // txtRecv
+            // 
+            this.txtRecv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRecv.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRecv.Location = new System.Drawing.Point(19, 353);
+            this.txtRecv.Multiline = true;
+            this.txtRecv.Name = "txtRecv";
+            this.txtRecv.Size = new System.Drawing.Size(614, 192);
+            this.txtRecv.TabIndex = 26;
+            // 
+            // cmdDisconnect
+            // 
+            this.cmdDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdDisconnect.Location = new System.Drawing.Point(639, 62);
+            this.cmdDisconnect.Name = "cmdDisconnect";
+            this.cmdDisconnect.Size = new System.Drawing.Size(136, 36);
+            this.cmdDisconnect.TabIndex = 29;
+            this.cmdDisconnect.Text = "Disconnect";
+            this.cmdDisconnect.UseVisualStyleBackColor = true;
+            this.cmdDisconnect.Click += new System.EventHandler(this.cmdDisconnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 24);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Received:";
+            // 
+            // cmdConnect
+            // 
+            this.cmdConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdConnect.Location = new System.Drawing.Point(639, 18);
+            this.cmdConnect.Name = "cmdConnect";
+            this.cmdConnect.Size = new System.Drawing.Size(136, 36);
+            this.cmdConnect.TabIndex = 28;
+            this.cmdConnect.Text = "Connect";
+            this.cmdConnect.UseVisualStyleBackColor = true;
+            this.cmdConnect.Click += new System.EventHandler(this.cmdConnect_Click);
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLogs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogs.Location = new System.Drawing.Point(3, 3);
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.Size = new System.Drawing.Size(788, 504);
+            this.txtLogs.TabIndex = 0;
+            this.txtLogs.Text = "";
+            this.txtLogs.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cmdClear3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 507);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(788, 43);
+            this.panel1.TabIndex = 1;
+            // 
+            // cmdClear3
+            // 
+            this.cmdClear3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdClear3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdClear3.Location = new System.Drawing.Point(647, 3);
+            this.cmdClear3.Name = "cmdClear3";
+            this.cmdClear3.Size = new System.Drawing.Size(136, 36);
+            this.cmdClear3.TabIndex = 33;
+            this.cmdClear3.Text = "Clear";
+            this.cmdClear3.UseVisualStyleBackColor = true;
+            this.cmdClear3.Click += new System.EventHandler(this.cmdClear3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 582);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Web Socket Client Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdClear2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button cmdClear1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button cmdSend;
+        private System.Windows.Forms.TextBox txtRecv;
+        private System.Windows.Forms.Button cmdDisconnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cmdConnect;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox txtLogs;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cmdClear3;
+    }
+}
+
---
+++ 11.Samples/WebSocketClientTest/Form1.cs
@@ -0,0 +1,237 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WebSocketSharp;
+
+#endregion
+
+namespace WebSocketClientTest
+{
+    public partial class Form1 : Form
+    {
+        #region Constructor
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private WebSocket ws = null;
+
+        #endregion
+
+        #region Form Load/Closing
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmdConnect.Enabled = true;
+            cmdDisconnect.Enabled = true;
+            cmdSend.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Disconnect();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void cmdDisconnect_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void cmdSend_Click(object sender, EventArgs e)
+        {
+            SendMsg();
+        }
+
+        private void cmdClear1_Click(object sender, EventArgs e)
+        {
+            ClearSendMsg();
+        }
+
+        private void cmdClear2_Click(object sender, EventArgs e)
+        {
+            ClearRecvMsg();
+        }
+
+        private void cmdClear3_Click(object sender, EventArgs e)
+        {
+            ClearLogMsg();
+        }
+
+        #endregion
+
+        #region WS Handlers
+
+        private void Ws_OnOpen(object sender, EventArgs e)
+        {
+            Log("WS OnOpen handler execute.");
+        }
+
+        private void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            // Cross thread wrapper.
+            Invoke(new MethodInvoker(delegate () {
+                string message = e.Data;
+                Log(string.Format("WS OnMessage: {0}", message));
+                txtRecv.Text = message;
+            }));
+        }
+
+        private void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+            //Reconnect(e.Code, e.Reason);
+            Log(string.Format("WS OnClose : Code:{0}, Reason:{1}", e.Code, e.Reason));
+        }
+
+        private void Ws_OnError(object sender, ErrorEventArgs e)
+        {
+            Log(string.Format("WS OnError: {0}", e.Message));
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Connect()
+        {
+            if (null != ws)
+            {
+                Log("Already create web socket.");
+
+                Log(string.Format("Ready State: {0}.", ws.ReadyState));
+                if (ws.ReadyState != WebSocketState.Open)
+                {
+                    Log("Current connection is not in open state. Try to disconnect before send message.");
+                }
+
+                return;
+            }
+            else
+            {
+                string url = txtUrl.Text;
+                if (string.IsNullOrWhiteSpace(url))
+                {
+                    MessageBox.Show("Please assign url.");
+                    return;
+                }
+
+                Log("Create new web socket connection.");
+
+                try
+                {
+                    ws = new WebSocket(url);
+                    ws.OnOpen += Ws_OnOpen;
+                    ws.OnMessage += Ws_OnMessage;
+                    ws.OnError += Ws_OnError;
+                    ws.OnClose += Ws_OnClose;
+                    ws.Connect();
+                }
+                catch (Exception ex)
+                {
+                    Log(string.Format("WS Open error: {0}", ex.Message));
+                }
+            }
+
+            cmdConnect.Enabled = (null == ws || ws.ReadyState != WebSocketState.Open);
+            cmdDisconnect.Enabled = true;
+            cmdSend.Enabled = !cmdConnect.Enabled;
+        }
+
+        private void Disconnect()
+        {
+            if (null != ws)
+            {
+                try
+                {
+                    ws.OnOpen -= Ws_OnOpen;
+                    ws.OnMessage -= Ws_OnMessage;
+                    ws.OnError -= Ws_OnError;
+                    ws.OnClose -= Ws_OnClose;
+                    ws.Close();
+                }
+                catch (Exception ex)
+                {
+                    Log(string.Format("WS Close error: {0}", ex.Message));
+                }
+                try 
+                {
+                    (ws as IDisposable).Dispose();
+                }
+                catch (Exception ex2) 
+                {
+                    Log(string.Format("Dispose error: {0}", ex2.Message));
+                }
+            }
+            ws = null;
+
+            Log("web socket connection is closed.");
+        }
+
+        private void SendMsg()
+        {
+            string msg = txtSend.Text;
+            if (string.IsNullOrWhiteSpace(msg))
+            {
+                MessageBox.Show("No message to send.");
+                return;
+            }
+
+            if (null == ws || ws.ReadyState != WebSocketState.Open)
+            {
+                MessageBox.Show("No Web socket connect.");
+                return;
+            }
+
+            Log(string.Format("Sending Message: {0}", msg));
+            ws.Send(msg);
+        }
+
+        private void ClearSendMsg()
+        {
+            txtSend.Text = string.Empty;
+        }
+
+        private void ClearRecvMsg()
+        {
+            txtRecv.Text = string.Empty;
+        }
+
+        private void ClearLogMsg()
+        {
+            txtLogs.Text = string.Empty;
+        }
+
+        private void Log(string msg)
+        {
+            string dt = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", 
+                System.Globalization.DateTimeFormatInfo.InvariantInfo);
+            string line = string.Format("{0} : {1}", dt, msg) + Environment.NewLine;
+            txtLogs.AppendText(line);
+        }
+
+        #endregion
+    }
+}

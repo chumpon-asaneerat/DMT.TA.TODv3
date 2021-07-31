ee8887cfce602899287c85c251106189cd490155
chumpon-asaneerat <chumponsenate@gmail.com>
2021-08-01 01:22:20 +0700

    Refactor DMT.TA.TODv4 #2208 - DMT.TAxTODv3   - DMT.Models project     - Implements Exchange (TAxTOD) model #1.

--- 02.Models/DMT.Models/Models/TAxTOD/TAAExchange.cs
+++ 02.Models/DMT.Models/Models/TAxTOD/TAAExchange.cs
@@ -10,6 +10,13 @@ using Newtonsoft.Json;
 
 namespace DMT.Models
 {
+    // http://localhost:8000/api/account/request/getlist
+    // Server data parameter
+    /*
+    {
+        "status": "R" 
+    }
+    */
     // Server data result.
     /*
     {
@@ -171,5 +178,176 @@ namespace DMT.Models
         /// <summary>Gets or sets ApproveRemark.</summary>
         [PropertyMapName("ApproveRemark")]
         public string ApproveRemark { get; set; }
+    }
+
+    // // http://localhost:8000/api/account/request/getdetail (as parameter)
+    /*
+    {
+        "requestid": 1 ,
+        "tsbId": "09"
+    }
+    */
+    // http://localhost:8000/api/account/request/getdetail (as result)
+    // http://localhost:8000/api/account/request/savedetail (as parameter)
+    // http://localhost:8000/api/account/request/approve (as parameter)
+    /*
+    {
+      "RequestId": 1,
+      "TSBId": "09",
+      "CurrencyDenomId": 5,
+      "CurrencyValue": 5000,
+      "CurrencyCount": 500
+    }
+    */
+    public class TAAExchangeDetail
+    {
+        /// <summary>Gets or sets RequestId.</summary>
+        [PropertyMapName("RequestId")]
+        public int? RequestId { get; set; }
+        /// <summary>Gets or sets TSBId.</summary>
+        [PropertyMapName("TSBId")]
+        public string TSBId { get; set; }
+
+        /// <summary>Gets or sets CurrencyDenomId.</summary>
+        [PropertyMapName("CurrencyDenomId")]
+        public int? CurrencyDenomId { get; set; }
+
+        /// <summary>Gets or sets CurrencyValue.</summary>
+        [PropertyMapName("CurrencyValue")]
+        public decimal? CurrencyValue { get; set; }
+
+        /// <summary>Gets or sets CurrencyCount.</summary>
+        [PropertyMapName("CurrencyCount")]
+        public decimal? CurrencyCount { get; set; }
+    }
+}
+
+// for TA App used
+namespace DMT.Models
+{
+    /*
+    {
+        "requestid": 1 ,
+        "tsbId": "08",
+        "exchangebht" : 20000,
+        "borrowbht" : 0,
+        "additionalbht" : 0,
+        "periodbegin" : null,
+        "periodend" : null,
+        "remark" : "ทดสอบ",
+        "finishflag" : 0 ,
+        "userid" : "00444" ,
+        "tranactiondate" : "2021-04-13:22:03.112Z" ,
+        "status" : "R"
+    }
+    */
+    public class TAASaveRequestHeader
+    {
+        /// <summary>Gets or sets RequestId.</summary>
+        [PropertyMapName("RequestId")]
+        public int? RequestId { get; set; }
+        /// <summary>Gets or sets TSBId.</summary>
+        [PropertyMapName("TSBId")]
+        public string TSBId { get; set; }
+
+        /// <summary>Gets or sets ExchangeBHT.</summary>
+        [PropertyMapName("ExchangeBHT")]
+        public decimal? ExchangeBHT { get; set; }
+        /// <summary>Gets or sets BorrowBHT.</summary>
+        [PropertyMapName("BorrowBHT")]
+        public decimal? BorrowBHT { get; set; }
+        /// <summary>Gets or sets AdditionalBHT.</summary>
+        [PropertyMapName("AdditionalBHT")]
+        public decimal? AdditionalBHT { get; set; }
+        /// <summary>Gets or sets PeriodBegin.</summary>
+        [PropertyMapName("PeriodBegin")]
+        public DateTime? PeriodBegin { get; set; }
+        /// <summary>Gets or sets PeriodBeginDateString.</summary>
+        [JsonIgnore]
+        public string PeriodBeginDateString
+        {
+            get
+            {
+                var ret = (!this.PeriodBegin.HasValue || this.PeriodBegin.Value == DateTime.MinValue) ?
+                    "" : this.PeriodBegin.Value.ToThaiDateTimeString("dd/MM/yyyy");
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>Gets or sets PeriodEnd.</summary>
+        [PropertyMapName("PeriodEnd")]
+        public DateTime? PeriodEnd { get; set; }
+        /// <summary>Gets or sets PeriodEndDateString.</summary>
+        [JsonIgnore]
+        public string PeriodEndDateString
+        {
+            get
+            {
+                var ret = (!this.PeriodEnd.HasValue || this.PeriodEnd.Value == DateTime.MinValue) ?
+                    "" : this.PeriodEnd.Value.ToThaiDateTimeString("dd/MM/yyyy");
+                return ret;
+            }
+            set { }
+        }
+        /// <summary>Gets or sets Remark.</summary>
+        [PropertyMapName("Remark")]
+        public string Remark { get; set; }
+
+        [PropertyMapName("UserId")]
+        public string UserId { get; set; }
+
+        /// <summary>Gets or sets TransactionDate.</summary>
+        [PropertyMapName("TransactionDate")]
+        public DateTime? TransactionDate { get; set; }
+        /// <summary>Gets or sets TransactionDateTimeString.</summary>
+        [JsonIgnore]
+        public string TransactionDateTimeString
+        {
+            get
+            {
+                var ret = (!this.TransactionDate.HasValue || this.TransactionDate.Value == DateTime.MinValue) ?
+                    "" : this.TransactionDate.Value.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+                return ret;
+            }
+            set { }
+        }
+
+        /// <summary>Gets or sets FinishFlag.</summary>
+        [PropertyMapName("FinishFlag")]
+        public int? FinishFlag { get; set; }
+        /// <summary>Gets or sets Status.</summary>
+        [PropertyMapName("Status")]
+        public string Status { get; set; }
+    }
+
+    /*
+    {
+        "requestid": 1 ,
+        "tsbId": "09",
+        "currencydenomid" : 6,
+        "currencyvalue" : 5000,
+        "currencycount" : 500
+    }
+    */
+    public class TAASaveRequestDetail
+    {
+        /// <summary>Gets or sets RequestId.</summary>
+        [PropertyMapName("RequestId")]
+        public int? RequestId { get; set; }
+        /// <summary>Gets or sets TSBId.</summary>
+        [PropertyMapName("TSBId")]
+        public string TSBId { get; set; }
+
+        /// <summary>Gets or sets CurrencyDenomId.</summary>
+        [PropertyMapName("CurrencyDenomId")]
+        public int? CurrencyDenomId { get; set; }
+
+        /// <summary>Gets or sets CurrencyValue.</summary>
+        [PropertyMapName("CurrencyValue")]
+        public decimal? CurrencyValue { get; set; }
+
+        /// <summary>Gets or sets CurrencyCount.</summary>
+        [PropertyMapName("CurrencyCount")]
+        public decimal? CurrencyCount { get; set; }
     }
 }

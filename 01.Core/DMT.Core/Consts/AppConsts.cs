00b9cfcf96cb6eaffb4d3ef50fb4662156046605
chumpon-asaneerat <chumponsenate@gmail.com>
2021-11-16 09:51:34 +0700

    Refactor DMT.TA.TODv4 #2268 - DMT.TAxTODv3   - Update App Version.   - Update history.txt   - Update version.history.txt

--- 01.Core/DMT.Core/Consts/AppConsts.cs
+++ 01.Core/DMT.Core/Consts/AppConsts.cs
@@ -20,8 +20,8 @@ namespace DMT
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "10111";
-                public static DateTime LastUpdate = new DateTime(2021, 10, 22, 16, 55, 00);
+                public static string Build = "10120";
+                public static DateTime LastUpdate = new DateTime(2021, 11, 16, 12, 00, 00);
             }
 
             public static class TOD
@@ -30,8 +30,8 @@ namespace DMT
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "10112";
-                public static DateTime LastUpdate = new DateTime(2021, 11, 11, 16, 02, 00);
+                public static string Build = "10120";
+                public static DateTime LastUpdate = new DateTime(2021, 11, 16, 12, 00, 00);
             }
 
             public static class Account
@@ -40,8 +40,8 @@ namespace DMT
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "10111";
-                public static DateTime LastUpdate = new DateTime(2021, 10, 22, 16, 55, 00);
+                public static string Build = "10120";
+                public static DateTime LastUpdate = new DateTime(2021, 11, 16, 12, 00, 00);
             }
 
             public static class PlazaConfig

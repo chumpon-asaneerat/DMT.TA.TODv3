59fb83c42089811e406f5279676ac387cef4189b
chumpon-asaneerat <chumponsenate@gmail.com>
2022-05-05 11:57:32 +0700

    Refactor DMT.TA.TODv4 #2331 - DMT.TAxTODv3   - DMT.TA.App project     - TSB Exchange process UI #10.

---
+++ 09.App/DMT.TA.App/TA/Controls/Elements/Exchange/TSBExchangeTransactionExEntry.xaml.cs
@@ -0,0 +1,47 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+using DMT.Configurations;
+using DMT.Controls;
+using DMT.Models;
+using DMT.Services;
+
+#endregion
+
+namespace DMT.TA.Controls.Elements
+{
+    /// <summary>
+    /// Interaction logic for TSBExchangeTransactionExEntry.xaml
+    /// </summary>
+    public partial class TSBExchangeTransactionExEntry : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TSBExchangeTransactionExEntry()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        /// <param name="value">The TSB Exchange Transaction.</param>
+        public void Setup(TSBExchangeTransaction value)
+        {
+            this.DataContext = value;
+        }
+
+        #endregion
+    }
+}

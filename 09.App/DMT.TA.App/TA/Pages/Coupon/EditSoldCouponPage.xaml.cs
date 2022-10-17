7dea208a8d35d7833b82adaef5105e9923a9aa8f
chumpon-asaneerat <chumponsenate@gmail.com>
2022-10-17 14:37:42 +0700

    Refactor DMT.TA.TODv4 #2427 - DMT.TAxTODv3   - TA App     - Implements EditSoldCoupon #2

---
+++ 09.App/DMT.TA.App/TA/Pages/Coupon/EditSoldCouponPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿#region Using
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using System.Reflection;
+
+using DMT.Models;
+using DMT.Services;
+
+using NLib;
+using NLib.Services;
+using NLib.Reflection;
+
+#endregion
+
+namespace DMT.TA.Pages.Coupon
+{
+    /// <summary>
+    /// Interaction logic for EditSoldCouponPage.xaml
+    /// </summary>
+    public partial class EditSoldCouponPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public EditSoldCouponPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        //private CultureInfo culture = new CultureInfo("th-TH") { DateTimeFormat = { Calendar = new ThaiBuddhistCalendar() } };
+        private CultureInfo culture = new CultureInfo("th-TH");
+        private XmlLanguage language = XmlLanguage.GetLanguage("th-TH");
+
+        private User _chief = null;
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Setup DateTime Picker
+            // Sold Data
+            //dtSoldDate.CultureInfo = culture;
+            //dtSoldDate.Language = language;
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}

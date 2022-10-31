23421d05725c0ed373596fd5930da5199461a87a
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-01 02:16:42 +0700

    Refactor DMT.TA.TODv4 #2446 - DMT.TAxTODv3   - Update TA Replace Coupon process #1.

---
+++ 02.Models/DMT.Models/Models/TAxTOD/TAAEditserialno.cs
@@ -0,0 +1,33 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using NLib.Reflection;
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace DMT.Models
+{
+    // Server data save(update)
+    /*
+    {
+        "tsbid": "09",
+        "oldserialno": "C000527",
+        "newserialno": "C000603"
+    }
+    */
+    public class TAAEditserialno
+    {
+        /// <summary>Gets or sets TSBId.</summary>
+        [PropertyMapName("TSBId")]
+        public string TSBId { get; set; }
+        /// <summary>Gets or sets Old Serial No.</summary>
+        [PropertyMapName("Oldserialno")]
+        public string Oldserialno { get; set; }
+        /// <summary>Gets or sets New Serial No.</summary>
+        [PropertyMapName("Newserialno")]
+        public string Newserialno { get; set; }
+    }
+}

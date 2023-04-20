f86cb9dd7489025143d4404c28f467daf126d14e
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-21 01:47:14 +0700

    Refactor DMT.TA.TODv4 #2516 - Update App Version

--- 01.Core/DMT.Core/Consts/AppConsts.cs
+++ 01.Core/DMT.Core/Consts/AppConsts.cs
@@ -20,8 +20,8 @@ namespace DMT
                 // common
                 public static string Version = AppConsts.Version;
                 public static string Minor = AppConsts.Minor;
-                public static string Build = "10233";
-                public static DateTime LastUpdate = new DateTime(2023, 3, 9, 8, 30, 00);
+                public static string Build = "10234";
+                public static DateTime LastUpdate = new DateTime(2023, 4, 21, 2, 0, 00);
             }
 
             public static class TOD

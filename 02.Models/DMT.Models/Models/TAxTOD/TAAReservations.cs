b4691c21dead7db8f8116d8f6db5d3d5afe6ca51
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-10 01:01:54 +0700

    Refactor DMT.TA.TODv4 #2552 - Implements TA Reservation #4

--- 02.Models/DMT.Models/Models/TAxTOD/TAAReservations.cs
+++ 02.Models/DMT.Models/Models/TAxTOD/TAAReservations.cs
@@ -38,4 +38,62 @@ namespace DMT.Models
         public string PrefixTxt { get; set; }
         public int RunningNo { get; set; }
     }
+
+    public class ReserveHead
+    {
+        public string basedate { get; set; }
+        public string movementtype { get; set; } = "311"; // Fixed
+        public string goodsrecipient { get; set; }
+        public string receivingstor { get; set; }
+        public string userid { get; set; }
+        public string postingdate { get; set; }
+        public string mat_slip { get; set; }
+        public string headertext { get; set; }
+
+        public List<ReserveItem> items { get; set; } = new List<ReserveItem>();
+    }
+
+    public class ReserveItem
+    {
+        public string itemnumber { get; set; }
+        public string goodsrecipient { get; set; }
+        public string materialnum { get; set;}
+        public int quantity { get; set; }
+        public string unit { get; set; } = "BOK"; // Fixed
+        public string plant { get; set; } = "1010"; // Fixed
+        public string fromstor { get; set; } = "CDMT"; // Fixed
+    }
+    /*
+
+{
+    "basedate": "20230826",
+    "movementtype": "311",
+    "goodsrecipient": "TADD20230001",
+    "receivingstor": "CCDD",
+    "userid": "20001",
+    "postingdate": null,
+    "mat_slip": null,
+    "headertext": null,
+    "items": [
+        { 
+            "itemnumber": "1", 
+            "goodsrecipient": "TADD20230001", 
+            "materialnum": "310000080",
+            "quantity": 100,
+            "unit": "BOK",
+            "plant": "1010",
+            "fromstor": "CDMT"
+        },
+        { 
+            "itemnumber": "2", 
+            "goodsrecipient": "TADD20230001", 
+            "materialnum": "310000035",
+            "quantity": 50,
+            "unit": "BOK",
+            "plant": "1010",
+            "fromstor": "CDMT"
+        }
+    ]
+}
+    */
 }
--- 09.App/DMT.TA.App/TA/Windows/Reservation/NewReservationWindow.xaml.cs
+++ 09.App/DMT.TA.App/TA/Windows/Reservation/NewReservationWindow.xaml.cs
@@ -9,11 +9,14 @@ using DMT.Models;
 using DMT.Services;
 using NLib.Services;
 using NLib.Reflection;
+using System.Linq;
 
 #endregion
 
 namespace DMT.TA.Windows.Reservation
 {
+    using ops = DMT.Services.Operations.TAxTOD.SAP2;
+
     /// <summary>
     /// Interaction logic for NewReservationWindow.xaml
     /// </summary>
@@ -48,9 +51,23 @@ namespace DMT.TA.Windows.Reservation
 
         #region Private Methods
 
+        private Storagelocation storage;
+        private ReserveRunningNo runningNo;
+
+        private void LoadMasterData()
+        {
+            var tsb = TSB.GetCurrent().Value();
+            var year = DateTime.Now.Year.ToString();
+            storage = ops.GetStorageLocations(tsb.TSBId).Value().FirstOrDefault();
+            Console.WriteLine(storage);
+            runningNo = ops.GetReservationCurrentRunningNo(tsb.TSBId, year).Value().FirstOrDefault();
+            Console.WriteLine(runningNo);
+        }
+
         private void LoadComboboxs()
         {
-
+            var couponTypes = ops.GetCouponMasters().Value();
+            cbCouponMasters.ItemsSource = couponTypes;
         }
 
         private void SaveReservationToQueue()
@@ -64,6 +81,7 @@ namespace DMT.TA.Windows.Reservation
 
         public void Setup()
         {
+            LoadMasterData();
             LoadComboboxs();
         }
 
